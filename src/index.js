140f9b33943c140497afd90cc953349fa87b922e
pro100mq <144606932+pro100mq@users.noreply.github.com>
2025-04-26 19:41:50 +0300

    hw

---
+++ src/index.js
@@ -0,0 +1,36 @@
+// 1
+// let time = 0;
+// const interval = setInterval(() => {
+//   time++;
+//   alert(time);
+//   if (time === 5) {
+//     clearInterval(interval);
+//   }
+// }, 1000);
+
+// 2
+const btnColor = document.querySelector("#btnColor");
+
+setInterval(() => {
+    const green = Math.round(Math.random() * (255 - 0) + 0);
+    const yellow = Math.round(Math.random() * (255 - 0) + 0);
+    const red = Math.round(Math.random() * (255 - 0) + 0);
+    const randomColor = `rgb(${green}, ${yellow}, ${red})`;
+    btnColor.style.backgroundColor = randomColor;
+  
+    btnColor.style.color = `rgb(${Math.round(
+      Math.random() * (255 - 0) + 0
+    )}, ${Math.round(Math.random() * (255 - 0) + 0)}, ${Math.round(
+      Math.random() * (255 - 0) + 0
+    )})`;
+}, 1000)
+
+
+const size = document.querySelector('#textSize');
+const minSize = 10;
+const maxSize = 40;
+
+setInterval(() => {
+    const randomSize = Math.round(Math.random() * (maxSize - minSize) + 1);
+    size.style.fontSize = `${randomSize}px`
+}, 1000)
